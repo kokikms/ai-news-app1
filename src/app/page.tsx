319653b82360aab2d07a3d105f7e319c7c7b3b81
kidokoryo1 <129704449+kidokoryo1@users.noreply.github.com>
2025-08-21 17:47:15 +0900

    EsLintによるビルドエラーの解消

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-// app/page.tsx
+// app/page.tsx （修正後）
+import mockNews from "@/data/mockNews.json";
 import NewsCard from "@/components/news/NewsCard";
 // import { fetchNews } from '@/utils/fetchRSS'
 
 export default function HomePage() {
   // const newsList = await fetchNews()
   // 仮データ読み込み
-  const newsList = require("@/data/mockNews.json") as Article[];
+  const newsList = mockNews as Article[];
   return (
     <div className="space-y-4">
       {newsList.map((item) => (
